4750dc8bfc5a21d6751e0c6348ae4975c9071508
Shivam Arora <Shivam.arora@engagedly.com>
2024-06-25 17:55:36 +0530

    tab bar changes

--- src/Navigation/Nav.tsx
+++ src/Navigation/Nav.tsx
@@ -4,33 +4,37 @@ import React from "react";
 import { Portal } from "react-native-paper";
 import { HomeScreen } from "../modules/Home";
 import { NewsFeed } from "../modules/NewsFeed";
+import TabBar from './TabBar'; // Adjust import path as necessary
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export const StackMore = createStackNavigator();
 export const Tab = createBottomTabNavigator();
 export const Nav = () => {
-
     return (
         <>
-           
+
             {/* <SafeAreaView style={{flex:0}}>
-                <SafeAreaView style={{flex: 1, position: 'relative'}}> */}
-                <Portal.Host>
-                    <Tab.Navigator screenOptions={{ headerShown: false }}>
-                        <Tab.Screen
-                            name="HomeScreen"
-                            component={HomeScreen}
-                        />
-                            <Tab.Screen name="Calender" component={NewsFeed} />
-                            <Tab.Screen name="Alerts" component={NewsFeed} />
-                            <Tab.Screen name="More" component={NewsFeed} />
-                          
-                           
-                    </Tab.Navigator>
-                </Portal.Host>
-                {/* </SafeAreaView>
+                <SafeAreaView style={{flex: 1, position: 'relative'}}> */}  
+                {/* <Tab.Navigator screenOptions={{ headerShown: false }}></Tab.Navigator> */}
+            <Portal.Host>
+                <Tab.Navigator
+                    screenOptions={({ route }) => ({
+                        tabBarActiveTintColor: '#0166DB',
+                        tabBarInactiveTintColor: '#201A1B',
+                        // headerShown: false
+                    })}
+                    tabBar={props => <TabBar {...props} />}
+                >
+                    <Tab.Screen name="Home" component={HomeScreen} />
+                    <Tab.Screen name="Newsfeed" component={NewsFeed} />
+                    <Tab.Screen name="Calendar" component={NewsFeed} />
+                    <Tab.Screen name="Alerts" component={NewsFeed} />
+                    <Tab.Screen name="More" component={NewsFeed} />
+                </Tab.Navigator>
+            </Portal.Host>
+            {/* </SafeAreaView>
                 </SafeAreaView> */}
-           
+
         </>
     );
 }
